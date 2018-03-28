d67ac64e3e556f740e15c89358d136c511da1284
Fuinha <marcos.marques@canaldapeca.com.br>
2018-03-28 15:23:10 -0300

    Fix timeout

--- src/bot-actions.js
+++ src/bot-actions.js
@@ -29,7 +29,8 @@ function spam(msg) {
         let spamMessage = words.slice(2).join(" ")
         if (spamMessage === '')
             spamMessage = 'Spamming you :pave:'
-        setTimeout(postMessage(msg.channel, spamMessage), i*666)
+        let timeout = i * 666
+        setTimeout(postMessage(msg.channel, spamMessage), timeout)
     }
 }
 
