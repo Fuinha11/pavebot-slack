3279d3017be18177d973db6a3572d7ddb4098fab
Fuinha <marcos.marques@canaldapeca.com.br>
2018-03-28 13:27:11 -0300

    Remove only mentions functionality

--- src/bot.js
+++ src/bot.js
@@ -13,7 +13,7 @@ bot.started((payload) => {
 
 bot.message((msg) => {
   if (!msg.user) return
-  if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return
+  // if (!_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) return
 
     let text = String(msg.text)
     console.log(text)
