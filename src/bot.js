76ac5d8ede92ce0436804518beb3e803ae09fbfd
Fuinha <marcos.marques@canaldapeca.com.br>
2018-03-29 10:34:57 -0300

    fix crashes, now report error

--- src/bot.js
+++ src/bot.js
@@ -13,24 +13,39 @@ bot.started((payload) => {
 })
 
 bot.message((msg) => {
-  if (!msg.user) return
-  if (_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) actions.mentions(msg)
+    try {
+        if (!msg.user) return
+        if (_.includes(msg.text.match(/<@([A-Z0-9])+>/igm), `<@${this.self.id}>`)) actions.mentions(msg)
 
-    let words = msg.text.split(" ")
+        let words = msg.text.split(" ")
 
-    switch (words[0]) {
-        case "!lol":
-            actions.lolBack(msg)
-            break
-        case "!email":
-            actions.emailBack(msg)
-            break
-        case "!spam":
-            actions.spam(msg)
-            break
-        case "!8ball":
-            actions.bolaOito(msg)
-            break
+        switch (words[0]) {
+            case "!lol":
+                actions.lolBack(msg)
+                break
+            case "!email":
+                actions.emailBack(msg)
+                break
+            case "!spam":
+                actions.spam(msg)
+                break
+            case "!8ball":
+                actions.bolaOito(msg)
+                break
+        }
+
+    } catch (e) {
+        slack.chat.postMessage({
+            token: config('SLACK_TOKEN'),
+            icon_emoji: config('ICON_EMOJI'),
+            channel: msg.channel,
+            username: 'PaveBot',
+            text: "''buguei \n" + e.toString() + "''"
+        }, (err, data) => {
+            if (err) throw err
+            let txt = _.truncate(data.message.text)
+            console.log(`🤖  beep boop: I responded with "${txt}"`)
+        })
     }
 
 })
