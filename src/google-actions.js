1216abe94254c0bb5f943283c0f97705f5d54f5c
Fuinha <marcos.marques@canaldapeca.com.br>
2018-04-16 11:46:45 -0300

    Add follow redirects to gSearch

--- src/google-actions.js
+++ src/google-actions.js
@@ -4,9 +4,17 @@
 const request = require('request');
 
 function luckySearch(searchName, callback) {
-    searchName = searchName.replace(" ", '+')
-    let url = 'http://www.google.com/search?q=' + searchName + '&btnI'
-    request(url, function (error, response, body) {
+    searchName = searchName.split(" ").join("+")
+    let url = 'http://www.google.com/search?q=' + searchName + '&safe=inactive&btnI'
+    const options = {
+        url: url,
+        method: 'GET',
+        followAllRedirects: true,
+        followRedirect: true
+    };
+    request(options, function (error, response, body) {
+        if (error)
+            console.log(error.toString())
         callback(this.uri.href)
     })
 }
@@ -21,19 +29,14 @@ function imageSearch(searchName, callback) {
 }
 
 function youtubeSearch(searchName, callback) {
-    searchName = searchName.replace(" ", '+')
-    let url = 'http://www.google.com/search?q=site%3Ayoutube.com+' + searchName + '&btnI'
-    request(url, function (error, response, body) {
-        callback(this.uri.href)
-    })
+    let url = 'site%3Ayoutube.com+' + searchName
+    luckySearch(url, callback)
 }
 
 function wikiSearch(searchName, callback) {
     searchName = searchName.replace(" ", '+')
-    let url = 'http://www.google.com/search?q=site%3Awikipedia.org+' + searchName + '&btnI'
-    request(url, function (error, response, body) {
-        callback(this.uri.href)
-    })
+    let url = 'site%3Awikipedia.org+' + searchName
+    luckySearch(url, callback)
 }
 
 function parseResponse(resp) {
